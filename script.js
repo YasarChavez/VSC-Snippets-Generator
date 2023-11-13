84b73b80a23c11a83c62c8737994f95b486949b8
YasarChavez <chavezyasar@gmail.com>
2023-11-13 19:38:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function generateSnippet() {
+    const snippetName = document.getElementById('snippetName').value;
+    const prefix = document.getElementById('prefix').value;
+    const description = document.getElementById('description').value;
+    const htmlCode = document.getElementById('htmlCode').value;
+  
+    const snippetCode = `"${snippetName}": {
+  "prefix": "${prefix}",
+  "body": [
+    ${JSON.stringify(htmlCode)}
+  ],
+  "description": "${description}"
+  }`;
+  
+    document.getElementById('snippetResult').textContent = snippetCode;
+  }
+  
+  function copySnippet() {
+    const snippetResult = document.getElementById('snippetResult');
+    const range = document.createRange();
+    range.selectNode(snippetResult);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert('Snippet copied to clipboard!');
+  }
+  
